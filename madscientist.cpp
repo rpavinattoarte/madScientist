ffc83702cef2b799aeede2d275d04f6ccc2f00ae
Rafael Pavinatto <75599124+rpavinattoarte@users.noreply.github.com>
2020-12-26 15:14:10 -0300

    Hello World - Intro the game

--- madscientist.cpp
+++ madscientist.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "\nYou are a scientist working on a secret project call 'tripleX'.\n";
+    std::cout << "\nPlease, enter the password to access the laboratory: \n";
+
+    //std::cout << "GAME OVER!";
     return 0;
 }

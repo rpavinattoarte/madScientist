aee352042a2912e03bb14e0d2fe8a108bba867e2
Rafael Pavinatto <75599124+rpavinattoarte@users.noreply.github.com>
2020-12-26 15:54:02 -0300

    Const  & Assigning Values

--- madscientist.cpp
+++ madscientist.cpp
@@ -5,9 +5,11 @@ int main()
     std::cout << "\nYou are a scientist working on a secret project call 'tripleX'.\n";
     std::cout << "\nPlease, enter the password to access the laboratory: ";
     //std::cout << "GAME OVER!";
-    int a = 4, b = 3, c = 2;
-    int sum = a + b + c;
-    int product = a * b * c;
+
+    
+    const int a = 4, b = 3, c = 2;
+    const int sum = a + b + c;
+    const int product = a * b * c;
     std::cout << "Sum " << sum << " Product " << product;
 
     return 0;

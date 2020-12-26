4f35ea8a3bc936c1673b3be18d9ed76329ce0924
Rafael Pavinatto <75599124+rpavinattoarte@users.noreply.github.com>
2020-12-26 17:33:42 -0300

    Getting User Input

--- madscientist.cpp
+++ madscientist.cpp
@@ -10,12 +10,24 @@ int main()
     const int codeA = 4, codeB = 3, codeC = 2;
     const int codeSum = codeA + codeB + codeC;
     const int codeProduct = codeA * codeB * codeC;
+    int playerGuess;
+    int guessSum;
+    int guessProduct;
+    int guessA, guessB, guessC;
+
+    //Inputs from prompt command
+    std::cin >> guessA;
+    std::cin >> guessB;
+    std::cin >> guessC;
+
+    guessSum = guessA + guessB + guessC;
+    guessProduct = guessA * guessB * guessC;
 
     //Return message from prompt command
     std::cout << "Print the Sum " << codeSum << std::endl;
     std::cout << "Print the Product " << codeProduct << std::endl;
     std::cout << "RESTRICT AREA! Insert the password 'Three numbers' to get access: ";
-    
 
+    
     return 0;
 }

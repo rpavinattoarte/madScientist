9f44ffc51e6779080bd495d251d6741c461fccae
Rafael Pavinatto <75599124+rpavinattoarte@users.noreply.github.com>
2020-12-26 15:39:10 -0300

    Variables - Sum and Product

--- madscientist.cpp
+++ madscientist.cpp
@@ -3,8 +3,12 @@
 int main()
 {
     std::cout << "\nYou are a scientist working on a secret project call 'tripleX'.\n";
-    std::cout << "\nPlease, enter the password to access the laboratory: \n";
-
+    std::cout << "\nPlease, enter the password to access the laboratory: ";
     //std::cout << "GAME OVER!";
+    int a = 4, b = 3, c = 2;
+    int sum = a + b + c;
+    int product = a * b * c;
+    std::cout << "Sum " << sum << " Product " << product;
+
     return 0;
 }

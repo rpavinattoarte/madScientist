89f33f5870ade95409f45c12308560ab8df01aed
Rafael Pavinatto <rpavinattoarte@gmail.com>
2020-12-27 14:15:20 -0300

    Functions

--- madscientist.cpp
+++ madscientist.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
     //Welcome message
     std::cout << "\nYou are a scientist working on a secret project call 'tripleX'.\n";
     std::cout << "\nPlease, enter the credentials to access the laboratory. \n";
     std::cout << "RESTRICT AREA! Insert the password 'Three numbers' to get access: " << std::endl;
+}
 
+void PlayGame()
+{
+  
     //Variables
     const int codeA = 4, codeB = 3, codeC = 2;
     const int codeSum = codeA + codeB + codeC;
@@ -28,6 +32,7 @@ int main()
     std::cout << "Print the Sum " << codeSum << std::endl;
     std::cout << "Print the Product " << codeProduct << std::endl;
 
+    //Check the password is correct
     if (guessSum == codeSum)
     {
         std::cout << "\nWelcome! Access Allowed." << std::endl;
@@ -36,6 +41,11 @@ int main()
     {
         std::cout << "Sorry, Access denied! Try again..." << std::endl;
     }
+}
 
+int main()
+{
+    PrintIntroduction();
+    PlayGame();
     return 0;
 }
